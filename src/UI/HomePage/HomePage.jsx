9f5449bfb991578e926a438b8525e97fa7b85cdf
Alexei Mozyrskiy <mozyrski@gmail.com>
2021-02-13 21:08:21 +0300

    failed attempts with two divs and contentEditable = true

--- src/UI/HomePage/HomePage.jsx
+++ src/UI/HomePage/HomePage.jsx
@@ -1,14 +1,26 @@
 import React from "react";
+import cn from "classnames";
 
-const HomePage = () => {
+const HomePage = (props) => {
     return (
-        <div class="page-wrapper">
-            <div class="task task-notActive">
-                <div class="task__rhombus"></div>           { /* Ромб который появлется с лева и потом прячется обратно за оверфлоу хидден */}
-                <div class="task__square task__square-notActive"></div>     {/* Квадрат, который появляется слева при активации формы */}
-                <textarea class="task__textarea task__textarea-notActive" placeholder="Write a new task"></textarea>
-                <div class="task__calendar-and-select-wrapper displayNone">
-                    <div class="task__calendar"><p class="icon-calendar"></p></div>
+        <div className="page-wrapper">
+            <div className={cn("task", {"task-notActive": !props.isTextareaActive}, {"task-active": props.isTextareaActive})} style={{height: props.taskHeight + 'px'}} >
+                <div className={cn("task__rhombus", {"task__rhombus-active": props.isTextareaActive})}></div>           { /* Ромб который появлется с лева и потом прячется обратно за оверфлоу хидден */}
+                <div className={cn("task__square", {"task__square-notActive": !props.isTextareaActive}, {"task__square-active": props.isTextareaActive})}></div>     {/* Квадрат, который появляется слева при активации формы */}
+                <div contentEditable="true"
+                    className={cn("task__textarea", {"task__textarea-notActive": !props.isTextareaActive}, {"task__textarea-active": props.isTextareaActive})}
+                    // placeholder="Write a new task"
+                    style={{height: props.textareaHeight + 'px'}}
+                    onClick={(e) => props.onTextareaClick(e.target.scrollHeight)}
+                    onInput={(e) => props.onTextareaChange(e.target.scrollHeight, e.target.innerText)}
+                    >{props.taskPlaceHolder === "" ? props.taskText : props.taskPlaceHolder}
+                    <div contentEditable="true"
+                        className="task__texarea2"
+                        style={{height: 70 + 'px'}}
+                    ></div>
+                </div>
+                <div className={cn("task__calendar-and-select-wrapper", {"displayNone": !props.isTextareaActive})}>
+                    <div className="task__calendar"><p className="icon-calendar"></p></div>
                     <select>
                         <option selected>No List</option>
                         <option>List 1</option>
--- src/UI/HomePage/HomePageContainer.jsx
+++ src/UI/HomePage/HomePageContainer.jsx
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import HomePage from "./HomePage";
+import { getIsTextAreaActiveSelector } from "../../BLL/textArea/selectors";
+import { toogleIsTextAreaActive } from "../../BLL/textArea/actionCreators";
+import { connect } from "react-redux";
 
-const HomePageContainer = () => {
+const HomePageContainer = (props) => {
+
+    let [textareaHeight, changeTextareaHeight] = useState(70);       // текущая высота текстареа, изначально получим ее при первом клике на текстарею, затем при изменении будем сравнивать прошлую высоту с нынешней и менять если не совпадут
+    let [taskHeight, changeTaskHeight] = useState(100);             // высота основного враппера, бем менять в зависимости от высоты текстареи(scrollHeight), чтобы полоса прокрутки не появлялась
+
+    let [taskText, changeTaskText] = useState("");                                  // то что печатаем
+    let [taskPlaceHolder, changeTaskPlaceHolder] = useState("Write a new task");        // Плейсхолдер для первого дива с таском который
+
+    function onTextareaClick(firstTextareaScrollHeight) {            // При клике на textArea
+        if(!props.isTextareaActive) {       // Если она не активна
+            props.toogleIsTextAreaActive(true); // сделаем ее активной
+            changeTextareaHeight(firstTextareaScrollHeight); // запишем первоночальную высоту текстареи (когда 1 строка) в переменную в стейте
+        }
+    }
+
+    function onTextareaChange(changedTextareaScrollHeight, value) {            // При изменении в textArea
+        
+        console.log("Полученное из инпута значение = ", value);
+        if(changedTextareaScrollHeight > textareaHeight) {             // если новыя высота текстареи больше предыдущей
+            changeTextareaHeight(changedTextareaScrollHeight);         // изменим текущую высоту в стейте, чтобы это условие выполнялось потом при дальнейшем изменении высоты, если юзер продолжит печатать и добавлять стороки
+            let newTaskDivHeight = taskHeight + 40;
+            changeTaskHeight(newTaskDivHeight);
+        } else if (changedTextareaScrollHeight < textareaHeight) {
+            changeTextareaHeight(changedTextareaScrollHeight);         // изменим текущую высоту в стейте, чтобы это условие выполнялось потом при дальнейшем изменении высоты, если юзер продолжит печатать и добавлять стороки
+            let newTaskDivHeight = taskHeight - 40;
+            changeTaskHeight(newTaskDivHeight);
+        }
+        console.log(taskText);
+    }
 
     return (
-        <HomePage />
+        <HomePage
+            isTextareaActive={props.isTextareaActive}
+            onTextareaClick={onTextareaClick}
+            onTextareaChange={onTextareaChange}
+            taskHeight={taskHeight}
+            textareaHeight={textareaHeight}
+            taskPlaceHolder={taskPlaceHolder}
+            taskText={taskText}
+        />
     );
 }
 
-export default HomePageContainer;
+const mapStateToProps = (state) => {
+    return {
+        isTextareaActive: getIsTextAreaActiveSelector(state)
+    }
+}
+
+export default connect(mapStateToProps, { toogleIsTextAreaActive })(HomePageContainer);
